601143c566be7ea19097dbc4cabad6d33fb40d88
ernnies <ernestnnamdie@gmail.com>
2024-11-21 20:53:26 +0100

    page up edits

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -56,7 +56,7 @@ const Home =
       const accounts = await ethereum.request({ method: 'eth_requestAccounts' })
 
       console.log('Found account', accounts[0])
-      setCurren
+      setCur
 
   //Checks if wallet is connected to the correct Network
   const checkCorrectNetwork = async () => {
