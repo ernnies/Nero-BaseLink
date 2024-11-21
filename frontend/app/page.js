642c6889b1ed6744f847dfaa7debb04132ee4d6f
ernnies <ernestnnamdie@gmail.com>
2024-11-21 20:50:33 +0100

    page up edits

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -57,7 +57,7 @@ const Home =
 
       console.log('Found account', accounts[0])
       setCurrentAccount(accounts[0])
-    } catch (erro
+    } catch (er
 
   //Checks if wallet is connected to the correct Network
   const checkCorrectNetwork = async () => {
