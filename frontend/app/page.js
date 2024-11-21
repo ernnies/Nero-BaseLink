9dc84bd1522e97c5f1452d28de866c0b909e6165
ernnies <ernestnnamdie@gmail.com>
2024-11-21 21:02:58 +0100

    page up edits

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -53,7 +53,7 @@ const Home =
         alert('You are not connected to the Sepolia testnet')
         return
       }
-      const accounts = await
+      const accounts = awa
 
   //Checks if wallet is connected to the correct Network
   const checkCorrectNetwork = async () => {
