e6ca307f26c2d9ebdd653e510727e73988b022fd
ernnies <ernestnnamdie@gmail.com>
2024-11-21 20:36:52 +0100

    page up edits

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -58,8 +58,7 @@ const Home =
       console.log('Found account', accounts[0])
       setCurrentAccount(accounts[0])
     } catch (error) {
-      console.log('Error connecting to metamask', error)
-    
+      console.log('Error connecting to metamask', error
 
   //Checks if wallet is connected to the correct Network
   const checkCorrectNetwork = async () => {
